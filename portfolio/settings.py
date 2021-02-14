9882422b973eb5681ddb00b8de648bc02af9ea97
Veeresh Halli <77663460+Veeresh-Halli@users.noreply.github.com>
2021-02-14 15:00:57 +0530

    Status_root added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -107,19 +107,21 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
