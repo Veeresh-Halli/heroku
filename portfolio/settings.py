7106f8985c1e24cc6ec1dacd5122d5c239cf0b79
Veeresh Halli <77663460+Veeresh-Halli@users.noreply.github.com>
2021-02-14 16:33:11 +0530

    Staticroot changed...and Prooc

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,9 +119,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
